2ccea37cb489e5932a092f5253b8e2eda48a3a76
atticusofsparta <atticusofsparta@protonmail.com>
2025-05-13 09:45:52 -0500

    fix(ario): add ardrive CU url and ario-process flag

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
-import { ANT, ARIO, ArweaveSigner } from '@ar.io/sdk';
+import { ANT, AOProcess, ARIO, ARIO_MAINNET_PROCESS_ID, ARIO_TESTNET_PROCESS_ID, ArweaveSigner } from '@ar.io/sdk';
 import fs from 'fs';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+import { connect } from '@permaweb/aoconnect';
+
 import TurboDeploy from './turbo';
 
+const arweaveTxIdRegex = /^[a-zA-Z0-9-_]{43}$/;
+
 const argv = yargs(hideBin(process.argv))
+	.option('ario-process', {
+		alias: 'p',
+		type: 'string',
+		description: 'The ARIO process to use',
+		demandOption: true,
+		default: ARIO_MAINNET_PROCESS_ID
+	})
 	.option('arns-name', {
 		alias: 'n',
 		type: 'string',
@@ -29,6 +40,12 @@ const argv = yargs(hideBin(process.argv))
 
 const DEPLOY_KEY = process.env.DEPLOY_KEY;
 const ARNS_NAME = argv.arnsName;
+let ARIO_PROCESS = argv.arioProcess;
+if (ARIO_PROCESS === 'mainnet') {
+	ARIO_PROCESS = ARIO_MAINNET_PROCESS_ID;
+} else if (ARIO_PROCESS === 'testnet') {
+	ARIO_PROCESS = ARIO_TESTNET_PROCESS_ID;
+}
 
 export function getTagValue(list, name) {
 	for (let i = 0; i < list.length; i++) {
@@ -42,6 +59,11 @@ export function getTagValue(list, name) {
 }
 
 (async () => {
+	if (!ARIO_PROCESS || !arweaveTxIdRegex.test(ARIO_PROCESS)) {
+		console.error('ARIO_PROCESS must be a valid Arweave transaction ID, or "mainnet" or "testnet"');
+		process.exit(1);
+	}
+
 	if (!DEPLOY_KEY) {
 		console.error('DEPLOY_KEY not configured');
 		process.exit(1);
@@ -69,7 +91,13 @@ export function getTagValue(list, name) {
 	}
 
 	const jwk = JSON.parse(Buffer.from(DEPLOY_KEY, 'base64').toString('utf-8'));
-	const ario = ARIO.init()
+	const ario = ARIO.init({process: new AOProcess({
+		processId: ARIO_PROCESS,
+		ao: connect({
+			MODE: 'legacy',
+			CU_URL:"https://cu.ardrive.io"
+		})
+	})});
 	const arnsNameRecord = await ario.getArNSRecord({name: ARNS_NAME}).catch((e) => {
 		console.error(`ARNS name [${ARNS_NAME}] does not exist`);
 		process.exit(1);

f2997f66b954fa550d74f85ed763a85fb1a878ec
Rani Elhusseini <charmful0x@gmail.com>
2026-06-07 12:22:40 +0200

    chore: format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,7 @@ export default defineConfig({
         'prompts/wallet': resolve(__dirname, 'src/prompts/wallet.ts'),
         'utils/config-resolver': resolve(__dirname, 'src/utils/config-resolver.ts'),
         'utils/constants': resolve(__dirname, 'src/utils/constants.ts'),
-        'utils/hyperbeam-uploaders-cli': resolve(
-          __dirname,
-          'src/utils/hyperbeam-uploaders-cli.ts',
-        ),
+        'utils/hyperbeam-uploaders-cli': resolve(__dirname, 'src/utils/hyperbeam-uploaders-cli.ts'),
         'utils/permawebos-bundlers': resolve(__dirname, 'src/utils/permawebos-bundlers.ts'),
         'utils/names': resolve(__dirname, 'src/utils/names.ts'),
         'utils/path': resolve(__dirname, 'src/utils/path.ts'),
